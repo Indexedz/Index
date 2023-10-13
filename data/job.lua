6bd5f654271892240c2215ce7a90b040ed8ab6a9
iwind <134185860+iwindd@users.noreply.github.com>
2023-10-14 03:38:55 +0700

    job permissions

--- data/job.lua
+++ data/job.lua
@@ -3,19 +3,20 @@ return {
     ['unemployed'] = {
       label = 'ว่างงาน',
       grades = {
-        { name = 'Freelancer', payment = 0 },
+        { name = 'Freelancer', payment = 0, permissions = {} },
       },
     },
     ['police'] = {
       label = 'Police',
       grades = {
-        { name = 'Police', payment = 50 },
+        { name = 'Police', payment = 50, permissions = {} },
       },
     },
     ['mechanic'] = {
       label = "ช่างยนต์",
       grades = {
-        { name = "อิสระ", payment = 0 },
+        { name = "อิสระ", payment = 0, permissions = {} },
+        { name = "boss", payment = 0, permissions = {} }
       }
     }
   }
